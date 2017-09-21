d0172627e1234344bf770040652efdd6e2936c14
Kazuhiko Shiozaki <kazuhiko@nexedi.com>
2017-09-21 10:23:44 +0200

    fixup! fixup! Formulator: prepare DateTimeField's sub_form dynamically.

--- product/ERP5Form/ProxyField.py
+++ product/ERP5Form/ProxyField.py
@@ -493,8 +493,10 @@ class ProxyField(ZMIField):
       field = template_field
     return template_field
 
-  def _get_sub_form(self):
-    return self.getTemplateField()._get_sub_form(field=self)
+  def _get_sub_form(self, field=None):
+    if field is None:
+      field = self
+    return self.getTemplateField()._get_sub_form(field=field)
 
   security.declareProtected('Access contents information',
                             'is_delegated')
